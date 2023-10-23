331678726f60f74754b35b3aca2f0dff799626c4
Dusty A <WildBamaBoy@users.noreply.github.com>
2023-10-23 11:34:53 -0500

    feat: player online checking for 7 days

--- Tebex-RCON/Plugins/SevenDaysPlugin.cs
+++ Tebex-RCON/Plugins/SevenDaysPlugin.cs
@@ -25,7 +25,34 @@ namespace Tebex.Plugins
 
         public override bool IsPlayerOnline(string playerId)
         {
-            return false;
+            _protocolManager.EnablePolling = false;
+            
+            _protocolManager.Write("listplayers");
+
+
+            bool found = false;
+            var cmdExecMessage = _protocolManager.Read();
+
+            while (true)
+            {
+                // After command exec message, this will be the first connected player
+                var message = _protocolManager.Read();
+
+                //TODO possible conflict with other commands that might be ran at the same time?
+                if (message.Contains("pltfmid=") && message.Contains(playerId))
+                {
+                    found = true;
+                    break;
+                }
+
+                if (message.Contains("Total of")) // End of player list is a total of the connected players
+                {
+                    break;
+                }
+            }
+            
+            _protocolManager.EnablePolling = true;
+            return found;
         }
         
         public override bool AuthenticateGame(string gameType)
@@ -66,13 +93,13 @@ namespace Tebex.Plugins
 
         public override object GetPlayerRef(string idOrUsername)
         {
-            throw new NotImplementedException();
-            return null;
+            // Ref will be the desired steamid
+            return idOrUsername;
         }
 
         public override string ExpandGameUsernameVariables(string cmd, object playerObj)
         {
-            throw new NotImplementedException();
+            return cmd;
         }
     }   
 }
