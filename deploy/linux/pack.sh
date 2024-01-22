533eb3bcd9f998f47aebefba4327b0808fb8e734
Dusty A <WildBamaBoy@users.noreply.github.com>
2024-01-22 14:24:44 -0600

    feat: add deployments for linux

---
+++ deploy/linux/pack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Sync the Linux release to a remote server, to build a snapshot image of a basic RCON adapter
+
+SNAPSHOT_SYSTEM="root@addr"
+VERSION="1.0.0-alpha.5"
+
+# Sync distribution files to the server used for snapshots
+rsync -rv ../../Tebex-RCON/bin/Release/net7.0/linux-x64 $SNAPSHOT_SYSTEM:/root/Tebex-RCONAdapter/
+rsync -v ./Tebex-RCONAdapter.service $SNAPSHOT_SYSTEM:/etc/systemd/Tebex-RCONAdapter.service
+rsync -v ./etc/motd $SNAPSHOT_SYSTEM:/etc/motd
+rsync -rv ./root/* $SNAPSHOT_SYSTEM:/root/
