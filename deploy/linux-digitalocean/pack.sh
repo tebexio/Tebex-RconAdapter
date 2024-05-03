ff9cfb8f3c9d3d00944e2b89836abd76da42a4a6
Dusty A <WildBamaBoy@users.noreply.github.com>
2024-05-03 10:53:20 -0500

    Update pack.sh

--- deploy/linux-digitalocean/pack.sh
+++ deploy/linux-digitalocean/pack.sh
@@ -5,7 +5,7 @@ SNAPSHOT_SYSTEM="root@addr"
 VERSION="1.0.0"
 
 # Sync distribution files to the server used for snapshots
-rsync -rv ../../Tebex-RCON/bin/Release/net7.0/linux-x64 $SNAPSHOT_SYSTEM:/root/Tebex-RCONAdapter/
+rsync -rv ../linux/ $SNAPSHOT_SYSTEM:/root/Tebex-RCONAdapter/linux-x64/
 rsync -v ./Tebex-RCONAdapter.service $SNAPSHOT_SYSTEM:/etc/systemd/system/Tebex-RCONAdapter.service
 rsync -v ./etc/motd $SNAPSHOT_SYSTEM:/etc/motd
 rsync -rv ./root/* $SNAPSHOT_SYSTEM:/root/
