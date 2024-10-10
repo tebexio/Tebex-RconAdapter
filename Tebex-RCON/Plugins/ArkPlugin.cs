e2283cd4e5dd2413ed31d1fd9a6cab1f5ea75e65
Dusty A <WildBamaBoy@users.noreply.github.com>
2024-10-10 08:14:20 -0500

    periodically update player list for ARK instead of per command

--- Tebex-RCON/Plugins/ArkPlugin.cs
+++ Tebex-RCON/Plugins/ArkPlugin.cs
@@ -6,25 +6,42 @@ namespace Tebex.Plugins
 {
     public class ArkPlugin : RconPlugin
     {
+        private String _lastPlayerList;
+        
         public ArkPlugin(TebexRconAdapter adapter) : base(adapter)
         {
+            TebexRconAdapter.ExecuteEvery(TimeSpan.FromSeconds(5), () =>
+            { 
+                _adapter.LogDebug("listplayers");
+                
+                if (_adapter.GetRcon() == null)
+                {
+                    _adapter.LogDebug("no rcon");
+                    return;
+                }
+                
+                var listPlayersCommand = _adapter.GetRcon().Send("listplayers");
+                RconPacket listPlayersResponse;
+                listPlayersResponse = _adapter.GetRcon().ReceiveNext();
+                
+                // Keep Alive packets seem to knock things out of order, we can just ignore when we grab the wrong response
+                // because we should be updating the list of players every few seconds anyway.
+                if (listPlayersResponse != null && !listPlayersResponse.Message.Contains("But no response!!"))
+                {
+                    _adapter.LogDebug("received player list: " + listPlayersResponse.Message);
+                    _lastPlayerList = listPlayersResponse.Message;
+                }
+            });
         }
 
         public override string GetPluginVersion()
         {
-            return "1.0.0";
+            return "1.0.1-DEV";
         }
 
         public override bool IsPlayerOnline(TebexApi.DuePlayer player)
         {
-            var listPlayersCommand = _rcon.Send("listplayers");
-            var listPlayersResponse = _rcon.ReceiveResponseTo(listPlayersCommand.Id, 100);
-            if (!listPlayersResponse.Item2.Equals("")) // error present
-            {
-                return false;
-            }
-
-            return  listPlayersResponse.Item1.Response.Message.Contains(player.UUID);
+            return _lastPlayerList.Contains(player.UUID);
         }
 
         public override string ExpandGameUsernameVariables(string cmd, object playerObj)
@@ -34,7 +51,7 @@ namespace Tebex.Plugins
         
         public override RconConnection CreateRconConnection(string host, int port, string password)
         {
-            return new TelnetRcon(_adapter, host, port, password);
+            return new RconConnection(_adapter, host, port, password);
         }
     }   
 }
